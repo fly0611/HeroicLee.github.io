b5faa243a24874b49199c9b2eaa26e51210eea80
HeroicLee <1628076350@qq.com>
2018-02-03 01:43:19 +0800

    站点更新:2018-02-03 01/43/19

---
+++ js/src/click.js
@@ -0,0 +1,17 @@
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) {if (window.CP.shouldStopExecution(2)){break;} var source = arguments[i]; for (var key in source) {if (window.CP.shouldStopExecution(1)){break;} if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } }
+window.CP.exitedLoop(1);
+ }
+window.CP.exitedLoop(2);
+ return target; };
+
+var OPTS = {
+    fill: 'none',
+    radius: 25,
+    strokeWidth: { 50: 0 },
+    scale: { 0: 1 },
+    angle: { 'rand(-35, -70)': 0 },
+    duration: 500,
+    left: 0,
+    top: 0,
+    easing: 'cubic.out'
+};
